01f9695df01b5ce9289bf786d40db846b7e06c3c
yusufthebdev <yusufthebdev17@gmail.com>
2026-01-17 03:16:51 -0800

    Image added to portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -85,10 +85,10 @@ export default function Portfolio() {
           <div className="flex-shrink-0">
             <div className="relative">
               <div className="absolute inset-0 bg-white/20 rounded-full blur-2xl"></div>
-              <img 
-                src="https://api.dicebear.com/7.x/lorelei/svg?seed=Owoyemi&backgroundColor=000000&flip=false"
+              <img  
+                src="/yusufimg.jpg"
                 alt="Owoyemi Yusuf"
-                className="relative w-48 h-48 md:w-64 md:h-64 rounded-full border-4 border-white/30 shadow-2xl bg-white/5"
+                className="relative w-48 h-48 md:w-64 md:h-64 rounded-full border-4 border-white/30 shadow-2xl bg-white/5 object-cover"
               />
             </div>
           </div>
